a95b6752cdbe460f408ac1330fcf38d9465bcd8c
Libardo Alejandro Quintero Gómez <quintero.libardo@correounivalle.edu.co>
2025-03-27 03:14:50 -0500

    Train move done

--- src/main/scala/ManiobrasTrenes/package.scala
+++ src/main/scala/ManiobrasTrenes/package.scala
@@ -3,4 +3,80 @@ package object ManiobrasTrenes {
   type Tren = List[Vagon]
   type Estado = (Tren, Tren, Tren)
 
+  trait Movimiento
+  case class Uno(n: Int) extends Movimiento
+  case class Dos(n: Int) extends Movimiento
+
+  type Maniobra = List[Movimiento]
+
+  def aplicarMovimiento(e: Estado, m: Movimiento): Estado = {
+
+    /*
+        def trenMenosDerecha(tren: Tren): Tren = tren match {
+          case t :: Nil => Nil
+          case t :: ts => (t)::(trenMenosDerecha(ts))
+        }
+
+        def trenMenosIzquierda(tren: Tren): Tren = tren match {
+          case t :: Nil => Nil
+          case t :: ts => ts
+        }
+
+        def sacarPorDerecha(tren: Tren): Vagon = tren match{
+          case t::Nil => t
+          case t::ts => sacarPorDerecha(ts)
+        }
+        def sacarPorIzquierda(tren: Tren): Vagon = tren match {
+          case t :: ts => t
+        }
+    */
+
+
+    def mover(n:Int, c:Int, ps:Tren, ss:Tren, nomueve:Tren): (Tren,Tren,Tren) = {
+      if(n>0 && n!=c){
+        ps match{
+          case Nil => (ps,ss,nomueve)
+          case x::xs=> mover(n,c+1,ps.init, (ps.last)::ss, nomueve)
+        }
+      }else if(n<0 && n!=c){
+        ss match {
+          case Nil => (ps,ss,nomueve)
+          case x :: xs => mover(n, c - 1, ps ++ List(ss.head), ss.tail, nomueve)
+        }
+      }else{
+        (ps,ss,nomueve)
+      }
+    }
+
+
+    m match {
+      case Uno(x) => x match {
+        case 0  => e
+        case x => 
+          val resultado = mover(x,0,e._1,e._2, e._3);
+          (resultado._1, resultado._2,resultado._3)
+
+      }
+
+      case Dos(x) => x match{
+        case 0 => e
+        case x =>
+          val resultado = mover(x,0,e._1,e._3, e._2);
+          (resultado._1, resultado._3,resultado._2)
+    }
+    }
+
+  }
+/*
+  def aplicarMovimientos(e: Estado, movs: Maniobra): List[Estado] = {
+    // Implementación aquí
+  }
+
+  def definirManiobra(t1: Tren, t2: Tren): Maniobra = {
+    // Dados dos trenes t1 (el original) y t2 (el deseado), devuelve la maniobra que se necesita hacer
+    // para pasar una estación de maniobras del estado (t1, List(), List()) al estado (t2, List(), List()).
+    // Implementación aquí
+  }
+  */
+ 
 }
---
+++ target/scala-3.3.5/test-classes/Pruebas.sc
@@ -0,0 +1,31 @@
+import ManiobrasTrenes._
+println("Todas las pruebas pasdasaron correctamente.")
+val e1 = (List('A', 'B', 'C', 'D'), Nil, Nil)
+
+// Prueba 1: Mover dos vagones a la segunda lista
+val e2 = aplicarMovimiento(e1, Uno(2))
+//assert(e2 == (List('A', 'B'), List('C', 'D'), Nil), s"Error en Prueba 1: $e2")
+
+// Prueba 2: Mover un vagón de la segunda lista de vuelta a la primera
+val e3 = aplicarMovimiento(e2, Uno(-1))
+
+val e32 = aplicarMovimiento(e3, Uno(-1))
+//assert(e3 == (List('A', 'B', 'D'), List('C'), Nil), s"Error en Prueba 2: $e3")
+
+// Prueba 3: Mover un vagón de la primera lista a la tercera lista
+val e4 = aplicarMovimiento(e3, Dos(1))
+
+val e7 = aplicarMovimiento(e4, Uno(-2))
+//assert(e4 == (List('A', 'B'), List('C'), List('D')), s"Error en Prueba 3: $e4")
+
+// Prueba 4: Intentar mover 0 vagones (debe mantenerse igual)
+val e5 = aplicarMovimiento(e4, Uno(0))
+//(e5 == e4, s"Error en Prueba 4: $e5")
+
+// Prueba 5: Intentar mover más vagones de los que hay disponibles en la primera lista
+val e6 = aplicarMovimiento(e5, Uno(5))
+//assert(e6 == (Nil, List('A', 'B', 'C'), List('D')), s"Error en Prueba 5: $e6")
+
+
+println("Todas las pruebas pasaron correctamente.")
+println("Todas las pruebas pasdasaron correctamente.")
